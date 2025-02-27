2c07c6f97e4f1ef1976e0e97c3970e27ce21e526
YilmazSedaghati <y.sedag@gmail.com>
2025-02-28 00:23:00 +0100

    feat: exercise started

---
+++ js/snack-1.js
@@ -0,0 +1,22 @@
+// Crea un array composto da 10 automobili.
+// Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e
+// alimentazione (benzina, diesel, gpl, elettrico, metano).
+// Dividi le automobili in 3 array separati: nel primo array solo le auto a
+// benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// Infine stampa separatamente i 3 array.
+
+// Input logico: creare tramite array i vari oggetti: infine creare delle variabili vuote e riempirle con gli oggetti con determinate caratteristiche
+
+const cars [
+    { marca: "Volkswagen", modello: "Golf", alimentazione: "benzina" },
+    { marca: "Ford", modello: "Fiesta", alimentazione: "benzina" },
+    { marca: "Bmw", modello: "Serie 5", alimentazione: "diesel" },
+    { marca: "Audi", modello: "A6", alimentazione: "diesel" },
+    { marca: "Dacia", modello: "Duster", alimentazione: "gpl" },
+    { marca: "Reanult", modello: "Clio", alimentazione: "gpl" },
+    { marca: "Seat", modello: "Leon", alimentazione: "metano" },
+    { marca: "Skoda", modello: "Octavia", alimentazione: "metano" },
+    { marca: "Tesla", modello: "Model 3", alimentazione: "elettrico" },
+    { marca: "BYD", modello: "Atto III", alimentazione: "elettrico" },
+];
+
